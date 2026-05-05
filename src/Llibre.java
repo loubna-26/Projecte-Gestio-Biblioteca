6865c5edc89345a11bcbef3e0e80630a35f8d8a1
Loubna <loubna.houta@insbaixcamp.cat>
2026-05-05 17:31:59 +0200

    Proyecto biblioteca inicial

---
+++ src/Llibre.java
@@ -0,0 +1,88 @@
+
+public class Llibre {
+
+	private int id;
+	private String titol;
+	private String autor;
+	private String categoria;
+	private int vecesPrestat = 0;
+	private boolean prestat;
+
+	/**
+	 * Constructor de la clase Llibre.
+	 * @param id Identificador único del libro
+	 * @param titol Título del libro
+	 * @param autor Autor del libro
+	 */
+	public Llibre(int id, String titol, String autor, String categoria) {
+		this.id = id;
+		this.titol = titol;
+		this.autor = autor;
+		this.categoria = categoria;
+		this.prestat = false;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getTitol() {
+		return titol;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public int getVecesPrestat() {
+		return vecesPrestat ;
+	}
+
+	/**
+	 * Indica si el libro está prestado.
+	 * @return true si está prestado, false si está disponible
+	 */
+	public boolean esPrestat() {
+		return prestat;
+	}
+
+	/**
+	 * Comprovar disponibilitat del llibre
+	 */
+	public boolean estaDisponible() {
+		return !prestat;
+	}
+
+	/**
+	 * Marca el libro como prestado.
+	 */
+	public void prestar() {
+		prestat = true;
+		vecesPrestat++;
+	}
+
+	/**
+	 * Marca el libro como devuelto.
+	 */
+	public void retornar() {
+		prestat = false;
+	}
+
+	/**
+	 * Modifica los datos del libro.
+	 * @param nouTitol nuevo título
+	 * @param nouAutor nuevo autor
+	 */
+	public void modificar(String nouTitol, String nouAutor) {
+		this.titol = nouTitol;
+		this.autor = nouAutor;
+	}
+
+	/**
+	 * Devuelve información del libro en formato texto.
+	 */
+	@Override
+	public String toString() {
+		return "[" + id + "] " + titol + " - " + autor + " - " + categoria + (prestat ? " No disponible" : " Disponible");
+	}
+}
---
+++ src/Usuari.java
@@ -0,0 +1,79 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Usuari {
+
+	private int id;
+	private String nom;
+	private List <Llibre> llibresPrestats;
+	private List<Prestec> historial;
+	private static final int MAX_LLIBRES = 3;
+
+	/**
+	 * Constructor de la clase Usuari.
+	 * @param id Identificador del usuario
+	 * @param nom Nombre del usuario
+	 */
+	public Usuari(int id, String nom) {
+		this.id = id;
+		this.nom = nom;
+		this.llibresPrestats = new ArrayList<>();
+		this.historial = new ArrayList<>();
+	}
+
+	//Getters 
+	public String getNom() {
+		return nom;
+	}
+	public int getId() {
+		return id;
+	}
+	public List <Llibre> getLlibresPrestats(){
+		return llibresPrestats;
+	}
+	public List<Prestec> getHistorial() {
+		return historial;
+	}
+
+	/**
+	 * Añade un libro al usuario si no supera el límite.
+	 * 
+	 * @param llibre libro a añadir
+	 * @return true si se añade correctamente, false si no
+	 */
+	public boolean agafarLlibre(Llibre llibre) {
+		if (llibresPrestats.size() < MAX_LLIBRES) {
+			llibresPrestats.add(llibre);
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Elimina un libro de la lista del usuario.
+	 * 
+	 * @param llibre libro a devolver
+	 */
+	public void retornarLlibre(Llibre llibre) {
+		llibresPrestats.remove(llibre);
+	}
+
+	/**
+	 * Muestra los libros que tiene el usuario.
+	 */
+	public void mostrarLlibres() {
+		for (Llibre l : llibresPrestats) {
+			System.out.println(l);
+		}
+	}
+
+	/**
+	 * Muestra el historial del libro
+	 */
+	public void mostrarHistorial() {
+		for (Prestec p : historial) {
+			System.out.println(p);
+		}
+	}
+}
