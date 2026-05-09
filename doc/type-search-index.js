9bef74128ceaa753ca9f9965c961becd7cf4aab2
Fatima <fatima.chari@insbaixcamp.cat>
2026-05-09 15:18:46 +0200

    Buidar main

--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Biblioteca"},{"p":"<Unnamed>","l":"GestorBiblioteca"},{"p":"<Unnamed>","l":"Llibre"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Prestec"},{"p":"<Unnamed>","l":"Usuari"}];updateSearchResults();
